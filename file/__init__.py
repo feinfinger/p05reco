3c957e645069b70db351a74fe8fbceb76d6049cb
fwilde <fabian.wilde@hzg.de>
2017-06-12 15:42:20 +0200

    Added kit log file parser to __init__.py

--- file/__init__.py
+++ file/__init__.py
@@ -5,6 +5,7 @@ Modules for file handling, espescially xtm style (.sli, .img etc) and h5 files.
 
 from p05tools.file.read_dat import read_dat
 from p05tools.file.parse_scanlog import parse_scanlog
+from p05tools.file.parse_kit_scanlog import parse_kit_scanlog
 from p05tools.file.idl_h5 import Idl2H5
 from p05tools.file.readh5 import readh5
 from p05tools.file.mkdir import mkdir
